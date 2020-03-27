9f7651e1708be74e5e2e6ed56a351121aa9718ce
med-abouchouar <abouchouar98@gmail.com>
2020-03-27 04:34:31 +0100

    commint to the new branch.

--- TP_Github/src/Corona.java
+++ TP_Github/src/Corona.java
@@ -4,6 +4,9 @@ public class Corona {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("#Stay-at-home please");
+		
+		// Code modified
+		System.out.println("#Get benifit from online courses");
 
 	}
 
